1188c83013484fcb98b0fd526af6be9f70da55e1
Lavan <techlavan2005@gmail.com>
2026-01-05 10:00:38 +0530

    Delete
    Deleted un nessasary Files Created by Default

--- estate-agent-app/src/App.test.js
+++ estate-agent-app/src/App.test.js
@@ -1,7 +1,6 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
-/* just updated this to a simple test*/
 test('renders without crashing', () => {
   render(<App />);
 });
